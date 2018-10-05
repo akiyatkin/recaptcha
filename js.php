e04c2931335681870363d4cc5895d9842e124e54
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-10-05 23:42:11 +0400

    fix

--- js.php
+++ js.php
@@ -6,9 +6,11 @@ use akiyatkin\boo\Cache;
 
 $conf = Config::get('recaptcha');
 
-$js = Cache::func( function () {
+/*$js = Cache::func( function () {
 	return file_get_contents('https://www.google.com/recaptcha/api.js?onload=grecaptchaOnload&render=explicit&hl=ru');
-}, [], ['akiyatkin\boo\Cache','getDurationTime'], array('last friday'));
+}, [], ['akiyatkin\boo\Cache','getDurationTime'], array('last friday'));*/
+
+$js = file_get_contents('https://www.google.com/recaptcha/api.js?onload=grecaptchaOnload&render=explicit&hl=ru');
 $js .= 'window.grecaptchaOnload=function(){ Event.fire("reCAPTCHA") };';
 
 return Ans::js($js);
