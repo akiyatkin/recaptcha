c77d251dc1ff64ff21ea74dba8d901633ff89979
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 10:10:26 +0400

    fix

--- reCAPTCHA.js
+++ reCAPTCHA.js
@@ -4,11 +4,24 @@ import { Config } from '/vendor/infrajs/config/Config.js'
 import { Controller } from '/vendor/infrajs/controller/src/Controller.js'	
 import { Layer } from '/vendor/infrajs/controller/src/Layer.js'
 
+/*
+Если события не нужны, то и не надо их использовать
+У событий
+- Количесто запусков
+- Независимое использование
+- Расширяемость новым кодом-обработчиками
+init() - 
+activate()
+on.ready
+
+*/
+
 let reCAPTCHA = {
     on: (...params) => Fire.on(reCAPTCHA, ...params),
     hand: (...params) => Fire.hand(reCAPTCHA, ...params),
     wait: (...params) => Fire.wait(reCAPTCHA, ...params),
     init: async (div, id, counter) => {
+        reCAPTCHA.activate()
 		div = document.getElementById(div)
         let tag = tag => div.getElementsByTagName(tag)[0]
         let form = tag('form')
@@ -37,18 +50,22 @@ let reCAPTCHA = {
         await reCAPTCHA.wait('ready')
         let sk = Config.get('recaptcha').sitekey
         return await grecaptcha.execute(sk, { action: action })
+    },
+    activate: async() => {
+        let sk = Config.get('recaptcha').sitekey
+        let src = 'https://www.google.com/recaptcha/api.js?render=' + sk
+        await CDN.js('recaptcha', src)
+        grecaptcha.ready(() => reCAPTCHA.on('ready'))
     }
 }
 
-reCAPTCHA.hand('init', async () => {
-    let sk = Config.get('recaptcha').sitekey
-    let src = 'https://www.google.com/recaptcha/api.js?render=' + sk
-    await CDN.js('recaptcha', src)
-    grecaptcha.ready(() => reCAPTCHA.on('ready'))
-}, '', [], true) 
+// reCAPTCHA.hand('activate', async () => {
+    
+// }, obj, '', [], true) 
+// obj - подписка на null-объект или на все объекты?
 
-//'', ['тег'], true - Имя, зависимости, куда - перыми после тега
-//'', ['тег'], false - Имя, зависимости, куда - последними после тега
-//'', [], true - Имя, зависимости, куда - перыми вообще
+// '', ['тег'], true - Имя, зависимости, куда - перыми после тега
+// '', ['тег'], false - Имя, зависимости, куда - последними после тега
+// '', [], true - Имя, зависимости, куда - перыми вообще
 
 export {reCAPTCHA}
