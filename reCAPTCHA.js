54f722a222a3005cd5adc494efa968bbde1d36b5
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 05:43:06 +0400

    fix

--- reCAPTCHA.js
+++ reCAPTCHA.js
@@ -1,16 +1,42 @@
 import { Fire } from '/vendor/akiyatkin/load/Fire.js'
 import { CDN } from '/vendor/akiyatkin/load/CDN.js'
 import { Config } from '/vendor/infrajs/config/Config.js'
+import { Controller } from '/vendor/infrajs/controller/src/Controller.js'	
+import { Layer } from '/vendor/infrajs/controller/src/Layer.js'
 
 let reCAPTCHA = {
     on: (...params) => Fire.on(reCAPTCHA, ...params),
     hand: (...params) => Fire.hand(reCAPTCHA, ...params),
-    get: (...params) => Fire.get(reCAPTCHA, ...params),
+    wait: (...params) => Fire.wait(reCAPTCHA, ...params),
+    init: async (div, id, counter) => {
+		div = document.getElementById(div)
+        let tag = tag => div.getElementsByTagName(tag)[0]
+        let form = tag('form')
+        let cls = cls => form.getElementsByClassName(cls)[0]
+		let iscontext = () => {
+			if (!Controller.ids[id]) return true
+			return Controller.ids[id].counter == counter
+		}
+		Layer.hand('submit', async (layer) => {
+            if (layer != Controller.ids[id]) return
+			if (!iscontext()) return
+            let token = await reCAPTCHA.execute('contacts')
+            if (!iscontext()) return
+            let name = "g-recaptcha-token";
+            let inp = cls(name)
+            if (!inp) {
+                inp = document.createElement("input")
+                inp.type = "hidden"
+                inp.class = inp.name = name 
+                form.appendChild(inp)
+            }
+            inp.value = token
+		})
+    },
     execute: async (action) => {
-        await reCAPTCHA.get('ready')
+        await reCAPTCHA.wait('ready')
         let sk = Config.get('recaptcha').sitekey
-        let token = await grecaptcha.execute(sk, { action: action })
-        return token
+        return await grecaptcha.execute(sk, { action: action })
     }
 }
 
